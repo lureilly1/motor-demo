798291942196b600cade3f01128d2247e1158c6b
lureilly1 <lureilly1@gmail.com>
2021-06-28 16:11:52 +0100

    demo finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
 import HeaderSection from './components/HeaderSection'
+import { CurrentSelections, Filter, Bar } from '@motor-js/core';
 
 const App = () => {
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
-const classes = useStyles();
 
   return (
      <Grid container spacing={3} >
@@ -24,20 +10,20 @@ const classes = useStyles();
           <HeaderSection />
         </Grid>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>
-          </Paper>
+          <CurrentSelections />
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}></Paper>
+        <Grid item xs={12}>
+          <Filter dimension={['Company Name']} label={'Company'} />
+          <Filter dimension={['Category']} label={'Category'} />
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}></Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}></Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}></Paper>
+        <Grid item xs={12}>
+          <Bar 
+            height={550}
+            cols={[
+              { qField: 'Company Name', qLabel: 'Company' },
+              { qField: '=Sum(Quantity)', qLabel: 'Quantity' },
+            ]} 
+          />
         </Grid>
       </Grid>
   )
